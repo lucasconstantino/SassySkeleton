a686ad6c11b27b1ec8ec209ad77e87f1391b7923
Lucas Constantino <lucas@taller.net.br>
2013-09-05 16:35:24 -0300

    Better organize comentaries on configuration file

--- config.rb
+++ config.rb
@@ -2,11 +2,29 @@
 # This file should walk you throw the configuration of the compass usage in your project.
 # For more detailed info, please refer to http://compass-style.org/help/tutorials/configuration-reference/
 
+
 # =================================
-# Structure
+# Environment
 # =================================
 
-# Set usage of relative paths (as opposed to absolute path) for assets like (e.g. images)
+# Change this to ":production" when ready to deploy the CSS to the live server.
+environment = :development
+
+# Set the syntax in usage.
+preferred_syntax = :sass
+
+# Select your output styles for development or production environments
+# (e.g. ":expanded", ":nested", ":compact" or ":compressed")
+development_output_style  = :expanded
+production_output_style   = :compressed
+
+
+
+# =================================
+# Directory Structure
+# =================================
+
+# Set usage of relative paths (as opposed to absolute path) for assets (e.g. images)
 relative_assets = true
 
 # Path locations.
@@ -20,33 +38,26 @@ sprite_load_path      = "../images-source"
 javascripts_dir       = "../scripts"
 fonts_dir             = "../fonts"
 
-# Add importing paths to use vendor's partials that reside outside the compass directory.
-# add_import_path "../../FOO/BAR"
 
-# Require any additional compass plugins installed on your system.
+
+# =================================
+# Vendor Settings
+# =================================
+
+# Add importing paths to use vendor's partials that reside outside the compass directory.
+# add_import_path '../../FOO/BAR'
+
+# Require any additional compass plugins.
 # require 'ninesixty'
 
 
-# =================================
-# Environment
-# =================================
-
-# Change this to ":production" when ready to deploy the CSS to the live server.
-environment = :development
-
-# Set the syntax in usage.
-preferred_syntax = :sass
-
-# Select your production output style (":expanded", ":nested", ":compact" or ":compressed")
-production_output_style = :compressed
-
 
 # =================================
-# You shouldn't need to change the configs below.
+# You shouldn't need to directives below.
 # =================================
 
 # Set environment dependant outputing style.
-output_style = (environment == :development) ? :expanded : production_output_style
+output_style = (environment == :development) ? development_output_style : production_output_style
 
 # Remove line comments depending on environment.
 line_comments = (environment == :development)
